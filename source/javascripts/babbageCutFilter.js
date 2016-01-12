2652dbcc010e9225bf4c6a61ff013e840b09516b
milafrerichs <mila.frerichs@gmail.com>
2016-01-12 11:50:56 +0100

    Rename einahmeausgabe to babbageCutFilter
    can be used with different cuts as well
    add a default cut, which will be changed and then the state gets updated

--- source/javascripts/babbageCutFilter.js
+++ source/javascripts/babbageCutFilter.js
@@ -1,4 +1,4 @@
-demo.directive('einahmeAusgabe', ['$rootScope', function($rootScope) {
+demo.directive('babbageCutFilter', ['$rootScope', function($rootScope) {
   return {
     restrict: 'EA',
     transclude: true,
@@ -7,9 +7,9 @@ demo.directive('einahmeAusgabe', ['$rootScope', function($rootScope) {
     scope: {
       selected: '@',
       filter: '=',
-      change: '&'
+      defaultCut: '='
     },
-    template: '<div class="einnahme-ausgabe"></div>',
+    template: '<div class="cut-filter"></div>',
     link: function(scope, element, attrs, babbageCtrl, transclude) {
       transclude(scope, function(clone, scope) {
         element.append(clone);
@@ -28,12 +28,27 @@ demo.directive('einahmeAusgabe', ['$rootScope', function($rootScope) {
           }
         }
       }
+      var findCutPosition = function(attrId) {
+        var cutLength = scope.defaultCut.length;
+        for(var i=0;i<cutLength;i++) {
+          var cut = scope.defaultCut[i].split(':');
+          var attr = attrId.split(':');
+          if(cut[0] == attr[0]) { return i; }
+        }
+        return -1;
+      }
+      var setNewCut = function(attrId) {
+        var pos = findCutPosition(attrId);
+        if(pos !== -1) {
+           scope.defaultCut[pos] = attrId;
+        }
+        return scope.defaultCut;
+      }
       scope.update = function(attr) {
         var state = babbageCtrl.getState();
-        state.cut = asArray(attr.id);
+        state.cut = setNewCut(attr.id);
         state.tile = [getParentTile(state.tile[0])];
         scope.selected = attr.label;
-        scope.change({attr: attr});
         babbageCtrl.setState(state);
       }
     }
