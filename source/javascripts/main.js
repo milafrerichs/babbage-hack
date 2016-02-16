eafa3bf2a7a5e9324bf3822eaea8a0fb6d026dcf
milafrerichs <mila.frerichs@gmail.com>
2016-02-16 11:17:13 +0100

    Only show content if name is set
    prevent undefined tooltip

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -109,7 +109,9 @@ demo.controller('DemoCtrl', function ($scope) {
   $scope.showTooltip = true;
   $scope.showTableKey = true;
   $scope.tooltipContent = function(d) {
-    return "<b>" + d._name + " ("+ addSpacingToKey(leadingZeros(d, d._key))+"):</b> <br/>" + d._area_fmt + " (" + percentFormat(d._percentage) + ")";
+    if(d._name) {
+      return "<b>" + d._name + " ("+ addSpacingToKey(leadingZeros(d, d._key))+"):</b> <br/>" + d._area_fmt + " (" + percentFormat(d._percentage) + ")";
+    }
   };
 
   $scope.setTile = function(tile) {
