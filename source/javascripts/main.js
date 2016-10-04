26b085aac0ca9ca22d4a0ae4747f7bcc46854031
milafrerichs <mila.frerichs@gmail.com>
2016-10-04 08:49:25 +0200

    Change colors

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -31,6 +31,10 @@ ngBabbageGlobals.categoryColors = [
     "#BCD631", "#95C93D", "#48B85C", "#00833D", "#00B48D",
     "#60C4B1", "#27C4F4", "#478DCB", "#3E67B1", "#4251A3", "#59449B",
     "#CF3D1E", "#F15623", "#F68B1F", "#FFC60B", "#DFCE21",
+    "#6E3F7C", "#6A246D", "#8A4873", "#EB0080", "#EF58A0", "#C05A89",
+    "#BCD631", "#95C93D", "#48B85C", "#00833D", "#00B48D",
+    "#60C4B1", "#27C4F4", "#478DCB", "#3E67B1", "#4251A3", "#59449B",
+    "#CF3D1E", "#F15623", "#F68B1F", "#FFC60B", "#DFCE21",
     "#6E3F7C", "#6A246D", "#8A4873", "#EB0080", "#EF58A0", "#C05A89"
     ];
 colorScale = d3.scale.ordinal().range(ngBabbageGlobals.categoryColors);
