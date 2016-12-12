a22ceb66627524e13998a9e7277818ec3237dd9a
milafrerichs <mila.frerichs@gmail.com>
2016-12-12 16:38:42 +0100

    Automated commit at 2016-12-12 15:38:42 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -68063,6 +68063,7 @@ demo.directive('treemapTable', ['$rootScope', '$http', function($rootScope, $htt
         if(newLevel) {
           currentState.tile = [ newLevel];
           currentState.cut = currentState.cut.concat([cut]);
+          currentState.tableOrder = '';
           babbageCtrl.setState(currentState);
         }
       };
